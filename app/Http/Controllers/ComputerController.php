cd3d0396956c1b385e21029f67e6de90aa2f5770
BayuDC <dev.bayudc@gmail.com>
2022-03-09 06:07:41 +0700

    Load computer data from db

--- app/Http/Controllers/ComputerController.php
+++ app/Http/Controllers/ComputerController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Computer;
 
 class ComputerController extends Controller {
     public function index() {
-        return view('computer.index');
+        return view('computer.index', [
+            'computers' => Computer::with('type')->get()
+        ]);
     }
 }
--- app/Models/Computer.php
+++ app/Models/Computer.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\ComputerType;
 
 class Computer extends Model {
     use HasFactory;
 
     public $timestamps = false;
+
+    public function type() {
+        return $this->belongsTo(ComputerType::class);
+    }
 }
