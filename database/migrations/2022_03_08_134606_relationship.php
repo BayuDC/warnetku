bc4ac4c25b538d5389c94f8c6413866b6f91703f
BayuDC <dev.bayudc@gmail.com>
2022-03-08 20:59:27 +0700

    Add table relationship migration

---
+++ database/migrations/2022_03_08_134606_relationship.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('computers', function (Blueprint $table) {
+            $table->foreign('type_id')->references('id')->on('computer_types');
+        });
+        Schema::table('rental_prices', function (Blueprint $table) {
+            $table->foreign('type_id')->references('id')->on('computer_types');
+        });
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->foreign('computer_id')->references('id')->on('computers');
+            $table->foreign('operator_id')->references('id')->on('operators');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('computers', function (Blueprint $table) {
+            $table->dropForeign('type_id');
+        });
+        Schema::table('rental_prices', function (Blueprint $table) {
+            $table->dropForeign('type_id');
+        });
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropForeign('computer_id');
+            $table->dropForeign('operator_id');
+        });
+    }
+};
