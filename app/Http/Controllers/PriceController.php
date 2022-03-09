d18db54e7c488e18a83155b77caf4e8633047233
BayuDC <dev.bayudc@gmail.com>
2022-03-09 09:31:26 +0700

    Load rental price data

--- app/Http/Controllers/PriceController.php
+++ app/Http/Controllers/PriceController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\RentalPrice;
 
 class PriceController extends Controller {
 
     public function index() {
-        return view('price.index');
+        return view('price.index', [
+            'gaming_prices' =>  RentalPrice::where('type_id', 1)->get(),
+            'office_prices' =>  RentalPrice::where('type_id', 2)->get()
+        ]);
     }
 }
