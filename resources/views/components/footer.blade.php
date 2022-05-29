2559ef3f0d95002807e6c6bc7d8b6424bf74c1cb
BayuDC <dev.bayudc@gmail.com>
2022-05-29 10:04:07 +0700

    Add footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-dark text-light text-center py-2">
+    <p class="m-0">
+        Made with ❤ by <a class="text-light" href="https://github.com/BayuDC">BayuDC</a>
+    </p>
+</div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,15 +12,18 @@
     <title>Warnetku</title>
 </head>
 
-<body>
+<body class="d-flex flex-column vh-100">
     <header>
-        @include('components.navbar')
+        @include('components.header')
     </header>
     <main>
         <div class="container my-4 px-sm-2 px-3">
             @yield('content')
         </div>
     </main>
+    <footer class="mt-auto">
+        @include('components.footer')
+    </footer>
 
     <!-- Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
